2167336ab8f749de1792ec8f9eac1a9de8c441b2
ayeshNipun <nipun.yesh@gmail.com>
2021-05-30 14:09:48 +0530

    Public folder and gitignore

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,43 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Header from "../components/header/header";
-import HeaderNoSidebar from "../components/header/headerNoSidebar";
-import Footer from "../components/footer/footer";
-import '../scss/style.scss';
-
+import React, { useState, useEffect } from "react"
+import CircleLoader from "react-spinners/CircleLoader";
+import PropTypes from "prop-types"
+import Header from "../components/header/header"
+import HeaderNoSidebar from "../components/header/headerNoSidebar"
+import Footer from "../components/footer/footer"
+import "../scss/style.scss"
 
 const Layout = ({ children }) => {
-  return (
-    <div className="active-dark-version main-wrapper">
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setDataLoaded(true)
+    }, 2000)
+  }, [])
+
+  if (!dataLoaded) {
+    return (
+      <div className="active-dark-version main-wrapper text-center">
+        <CircleLoader />
+      </div>
+    )
+  }
+
+  if (dataLoaded) {
+    return (
+      <div className="active-dark-version main-wrapper">
         <Header />
         <HeaderNoSidebar />
         <div className="home-content-wrapper">
-            <main>{children}</main>
-            <Footer />
+          <main>{children}</main>
+          <Footer />
         </div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
 }
 
 export default Layout
-
